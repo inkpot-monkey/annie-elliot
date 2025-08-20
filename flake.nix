8ed141707cb3f3f4bb3fcb8a7b2e34323c5c2a76
thomassdk <thomassdk@pm.me>
2025-08-20 16:40:12 +0200

    feat: init nix

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "A Nix-flake-based pnpm typescript development environment";
+
+  # Make sure packages are in the binary cache
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (builtins) attrValues;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+            inherit system;
+          }
+        );
+
+    in
+    {
+      packages = forEachSupportedSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.callPackage ./spinframework/package.nix { };
+        }
+      );
+
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShell {
+            packages = attrValues {
+              # Nix
+              inherit (pkgs) nil nixfmt;
+
+              # Language Servers
+              inherit (pkgs) yaml-language-server vscode-langservers-extracted;
+              inherit (pkgs.nodePackages)
+                typescript-language-server
+                bash-language-server
+                ;
+
+              # Node
+              inherit (pkgs) nodejs_24;
+              inherit (pkgs.nodePackages) pnpm;
+
+              # Python (for stylelint)
+              inherit (pkgs) python3;
+
+              inherit (self.packages.${system}) default;
+            };
+          };
+        }
+      );
+    };
+}
