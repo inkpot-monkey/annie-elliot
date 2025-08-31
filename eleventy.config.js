c76e82ac3b6ba93558978078aa3a64fb69835885
thomassdk <thomassdk@pm.me>
2025-08-31 19:24:51 +0200

    feat: add eleventy image plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,29 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import { eleventyImagePlugin } from "@11ty/eleventy-img";
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
   eleventyConfig.ignores.add("*.md");
 
   eleventyConfig.addPlugin(pluginWebc, {
-    components: ["./src/_components/**/*.webc"],
+    components: [
+      "./src/_components/**/*.webc",
+      "npm:@11ty/eleventy-img/*.webc",
+    ],
   });
 
   eleventyConfig.setServerOptions({
     domDiff: false,
+  });
+
+  eleventyConfig.addPlugin(eleventyImagePlugin, {
+    formats: ["webp", "jpeg"],
+    urlPath: "/img/",
+
+    defaultAttributes: {
+      loading: "lazy",
+      decoding: "async",
+    },
   });
 }
 
